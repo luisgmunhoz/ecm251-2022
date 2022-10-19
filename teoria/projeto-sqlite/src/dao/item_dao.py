46cf947fd64488bed7074df2d9d97718dca50d16
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-10-19 10:24:23 -0300

    item_dao.py

---
+++ teoria/projeto-sqlite/src/dao/item_dao.py
@@ -0,0 +1,28 @@
+import sqlite3
+from src.models.item import Item
+
+class ItemDAO:
+    _instance = None
+    
+    def __init__(self) -> None:
+        self._connect()
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = ItemDAO()
+        return cls._instance
+
+    def _connect(self):
+        self.conn = sqlite3.connect('./databases/sqlite.db')
+
+    def get_all(self):
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("""
+            SELECT * FROM Itens;
+        """)
+        resultados = []
+        for resultado in self.cursor.fetchall():
+            resultados.append(Item(id = resultado[0], nome = resultado[1], preco = resultado[2]))
+        self.cursor.close()
+        return resultados
