b5e03348257e09320084bb02d8aa00453c450363
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-10-26 11:41:25 -0300

    codigo aula

--- teoria/projeto-sqlite/src/controllers/pedido_controller.py
+++ teoria/projeto-sqlite/src/controllers/pedido_controller.py
@@ -1,34 +1,23 @@
+from src.dao.pedido_dao import PedidoDAO
 from src.models.pedido import Pedido
 from src.controllers.item_controller import ItemController
-from src.dao.pedido_dao import PedidoDAO
-
 class PedidoController:
     def __init__(self) -> None:
         pass
-    
     def total_pedido(self, numero_pedido) -> float:
-        items_pedido = PedidoDAO.get_instance().get_itens(numero_pedido)
+        items_pedido = PedidoDAO.get_instance().pegar_pedido(numero_pedido)
         total = 0
         item_controller = ItemController()
-        for (item_id, quantidade) in items_pedido:
-            item = item_controller.pegar_item(item_id)
-            total += item.preco * quantidade
-            
-    def pegar_pedido(self, numero_pedido) -> list[Pedido]:
-        pedido = PedidoDAO.get_instance().pegar_pedido(numero_pedido)
-        return pedido
+        for item in items_pedido:
+            item_elemento = item_controller.pegar_item(item.id_item)
+            total += item_elemento.preco * item.quantidade
+        return total
+    def pegar_pedido(self, numero_pedido)-> list[Pedido]:
+        return PedidoDAO.get_instance().pegar_pedido(numero_pedido)
 
-    def atualizar_pedido(self, pedido) -> bool:
+    def atualizar_pedido(self, pedido)-> bool:
         return PedidoDAO.get_instance().atualizar_pedido(pedido)
-
-    def deletar_pedido(self, id) -> bool:
+    def deletar_pedido(self, id) ->bool:
         return PedidoDAO.get_instance().deletar_item(id)
-    
-    def inserir_pedido(self, pedido) -> None:
+    def inserir_pedido(self, pedido)->None:
         PedidoDAO.get_instance().inserir_pedido(pedido)
-            
-    def pegar_todos_itens(self) -> list[Pedido]:
-        itens = PedidoDAO.get_instance().get_all()
-        return itens
-    
-    
