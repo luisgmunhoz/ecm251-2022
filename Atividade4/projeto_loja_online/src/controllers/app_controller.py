a74c9e001ecc147d518479e38afa03a4ce0379a5
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-11-12 12:29:11 -0300

    adicionando meu nome e RA nos daos e no style.css

--- Atividade4/projeto_loja_online/src/controllers/app_controller.py
+++
@@ -1,2 +0,0 @@
-# Luis Guilherme de Souza Munhoz RA: 20.01937-8
-## falta adicionar os views do MVC
--- Atividade4/projeto_loja_online/src/dao/product_dao.py
+++ Atividade4/projeto_loja_online/src/dao/product_dao.py
@@ -1,3 +1,5 @@
+# Luis Guilherme de Souza Munhoz RA: 20.01937-8
+
 import sqlite3
 from src.models.product import Product
 
